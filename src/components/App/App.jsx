582c2f5b1d10cac5ee6d137578c4fa7737608677
CHaPiOn777 <89907581+CHaPiOn777@users.noreply.github.com>
2022-06-25 21:39:53 +0300

    first commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import AppHeader from '../AppHeader/AppHeader';
+import stylesApp from './App';
+import './App.css';
+
+function App() {
+  return (
+    <div className={stylesApp.page}>
+      <AppHeader />
+
+    </div>
+  );
+}
+
+export default App;
