d3f1cb428e7376d66db155ba324ba847a844239e
CHaPiOn777 <89907581+CHaPiOn777@users.noreply.github.com>
2022-08-24 21:44:16 +0300

    В процессе реализиции изменения данных в личном кабинете

--- src/services/action/authAction.jsx
+++ src/services/action/authAction.jsx
@@ -1,27 +1,33 @@
 import { authUser } from "../../components/utils/burger-api";
+import { setCookie } from "../../components/utils/utils";
 import { LOADER } from "./orderDetailsAction";
 
 export const GET_AUTH_SUCCESS = 'GET_AUTH_SUCCESS';
 export const GET_AUTH_FAILED = 'GET_AUTH_FAILED';
 
 export const authAction = (email, password) => {
-  
+
   return function (dispatch) {
     dispatch({
       type: LOADER
     })
     authUser(email, password)
       .then(res => {
+        const accessToken = res.accessToken.split('Bearer ')[1];
+        const refreshToken = res.refreshToken;
+        setCookie('token', accessToken);
+        localStorage.setItem('refreshToken', refreshToken);
+        return res;
+      })
+      .then(res => {
         if (res && res.success) {
           dispatch({
             type: GET_AUTH_SUCCESS,
             user: res.user,
-            success: res.success,
-            accessToken: res.accessToken,
-            refreshToken: res.refreshToken
+            success: res.success
           })
         } else {
-          dispatch ({
+          dispatch({
             type: GET_AUTH_FAILED
           })
         }
